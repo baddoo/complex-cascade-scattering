3770225b261dd1f64065d76027c8de733d9de493
Peter Baddoo <peter@baddoo.org>
2020-02-13 12:35:14 +0000

    Adds tests for asymptotic roots

--- asymTest.m
+++ asymTest.m
@@ -1,17 +1,61 @@
-n3 = permute(1:5e3,[1,3,2]);
-a = 1.5; b = .85; c = 0; d =2;
-an3 = a*n3 + b + c*log(n3) + d./n3;
-nz = 100;
-z = linspace(0,100,nz)*exp(-1i*pi/4);
-exact1 = prod((1-bsxfun(@rdivide,z,an3)).*exp(bsxfun(@rdivide,z,an3)),3);
-%exact2 = prod((1-z./(a*n3+b)),3);
-approx = exp((-z./a+b./a+.5).*log(-a./z)-z./a*(1-eulergamma)+z.*sum(1./an3-1./(a*n3),3));
-%approx = exp(-(-z./a+b./a+.5).*log(-z./a)-z./a*(1-eulergamma)+z.*sum(-(b+d./n3)./(a*n3.*(a*n3+b+d./n3)),3));
+% Tests whether the asymptotic approximations for the roots are correct.
 
-%myProd = prod((an3-z)./(a*n3+b+c*log(n3)-z),3);
+AAData.sigma5 = 3*pi/4;
+AAData.omega5 = 10;
+AAData.w = 3;
+d = .9; s = .5;
+ADData.spac = [s,d];
+ADData.chie = atan(d/s);
+Modes.trunc = 400;
 
-%semilogy(abs(z),abs(exact1))
-plot(abs(exact1./approx))
-hold on
-%semilogy(abs(approx))
+tol = 1e-3;
+for j = 1:3
+
+    figure(j)
+    clf
+    hold on
+switch j
+    case 1
+        ADData.mu = [rand + 1i*rand 0 0];  
+        title('asymptotic approximation errors for case I boundary condition','interpreter','latex','fontsize',20)
+    case 2
+        ADData.mu = [rand+1i*rand rand+1i*rand 0];
+        title('asymptotic approximation errors for case II boundary condition','interpreter','latex','fontsize',20)
+    case 3
+       ADData.mu = [rand+1i*rand rand+1i*rand rand+1i*rand];   
+       title('asymptotic approximation errors for case III boundary condition','interpreter','latex','fontsize',20)
+
+end
+    
+f = @(xVar) 1./KNumlogD(xVar,ADData,AAData);
+logD = @(xVar) KNumlogD(xVar,ADData,AAData);
+
+asympGuess = computeAsympGuess(ADData,AAData,Modes);
+knownRootsInit = newtonKRoots(asympGuess,f,logD,tol,[]);
+
+% Divide into quadrants
+err = abs(asympGuess - knownRootsInit);
+err1 = err(0<angle(asympGuess) & angle(asympGuess)<pi/2);
+err2 = err(pi/2<angle(asympGuess) & angle(asympGuess)<pi);
+err3 = err(0>angle(asympGuess) & angle(asympGuess)>-pi/2);
+err4 = err(-pi<angle(asympGuess) & angle(asympGuess)<-pi/2);
+
+q1 = semilogy(err1,'-','LineWidth',3);
+q2 = semilogy(err2,'-','LineWidth',3);
+q3 = semilogy(err3,'-','LineWidth',3);
+q4 = semilogy(err4,'-','LineWidth',3);
+
+grid on
+
+ylabel('error','Interpreter','latex','FontSize',20)
+xlabel('$n$','Interpreter','latex','FontSize',20)
+
+set(gca,'YScale','log')
+
+legend('quadrant 1','quadrant 2','quadrant 3','quadrant 4')
+
+
 hold off
+
+
+end
